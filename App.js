c7c41acdd3e8fc5be22f1a745ffda0383fa90355
Taleh Guryanov <taleh.gurjanov@gmail.com>
2024-01-11 21:33:07 +0400

    added ImageBackground and LinearGradient

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import {StartGameScreen} from "./screens/StartGameScreen";
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
+      <LinearGradient style={styles.root} colors={['#4e0329', '#ddb25f']}>
+        <ImageBackground
+            source={require('./assets/images/background.png')}
+            style={styles.root}
+            resizeMode={"cover"}
+            imageStyle={styles.backgroundImage}
+        >
+          <StartGameScreen />
+        </ImageBackground>
+      </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
