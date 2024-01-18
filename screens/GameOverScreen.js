aef406a75120f76f132f3dd8478feab281210c55
Taleh Guryanov <taleh.gurjanov@gmail.com>
2024-01-18 23:27:39 +0400

    added useWindowDimensions for GameOverScreen.js

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,49 +1,71 @@
-import {View, StyleSheet, Image, Dimensions} from "react-native";
+import {View, StyleSheet, Image, useWindowDimensions, ScrollView} from "react-native";
 import {Title} from "../components/Title";
 import {PrimaryButton} from "../components/PrimaryButton";
 import {COLORS} from "../constants/colors";
 import {InstructionText} from "../components/InstructionText";
+import {useMemo} from "react";
 
 export const GameOverScreen = ({rounds, userNumber, onRestart}) => {
+  const {width, height} = useWindowDimensions();
+  
+  let imageSize = 300;
+  
+  const imageStyles = useMemo(() => {
+    console.log(width, height);
+    if (width < 430) {
+      imageSize = 150;
+    }
+  
+    if(height < 450) {
+      imageSize = 100;
+    }
+    
+    return {
+      borderRadius: imageSize / 2,
+      width: imageSize,
+      height: imageSize,
+    }
+  },[width, height]);
+  
   return (
-      <View style={styles.screen}>
-        <Title>GAME OVER</Title>
-        <View style={styles.imageWr}>
-          <Image
-              source={require('../assets/images/success.png')}
-              style={styles.image}
-          />
-        </View>
-        <View style={styles.textWr}>
+      <ScrollView style={styles.screen}>
+        <View style={styles.root}>
+          <Title>GAME OVER</Title>
+          <View style={[styles.imageWr, imageStyles]}>
+            <Image
+                source={require('../assets/images/success.png')}
+                style={styles.image}
+            />
+          </View>
+          <View style={styles.textWr}>
             <InstructionText style={styles.text}>
               You needed
               <InstructionText style={[styles.text, styles.textBold]}> {rounds} </InstructionText>
               rounds to guess the number
               <InstructionText style={[styles.text, styles.textBold]}> {userNumber} </InstructionText>
             </InstructionText>
+          </View>
+          <View style={styles.btnWr}>
+            <PrimaryButton onPress={onRestart}>Start New Game</PrimaryButton>
+          </View>
         </View>
-        <View style={styles.btnWr}>
-          <PrimaryButton onPress={onRestart}>Start New Game</PrimaryButton>
-        </View>
-      </View>
+      </ScrollView>
   );
 }
 
-const deviceWidth = Dimensions.get('window').width;
-
 const styles = StyleSheet.create({
   screen: {
+    flex: 1,
+  },
+  root: {
     flex: 1,
     padding: 24,
     alignItems: 'center',
     justifyContent: 'center',
   },
   imageWr: {
-    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderColor: COLORS.primary4,
     borderWidth: 3,
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
     overflow: 'hidden',
     marginVertical: 36,
   },
